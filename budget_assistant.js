63c70976e77c518428b6c4020fe3c5fed29fa170
OKent748 <kent13@usf.edu>
2025-05-27 18:14:53 -0400

    Add files via upload

---
+++ budget_assistant.js
@@ -0,0 +1,11 @@
+const startupName = "HighTech Company Inc";
+const initialCapital = 10000;
+const monthlyRevenue = 12000;
+const monthlyExpenses = 6000;
+const netCashFlow = monthlyRevenue-monthlyExpenses;
+const currentBalance = initialCapital+netCashFlow;
+const isProfitable = netCashFlow > 0;
+console.log("Startup Name: " + startupName);
+console.log("Net Monthly Cash Flow: $" + netCashFlow);
+console.log("Current Balance: $" + currentBalance);
+console.log("profitability: " + isProfitable);
